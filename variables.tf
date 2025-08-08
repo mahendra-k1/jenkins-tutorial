e5bd0d8339ea3dd15d3786a034d8d85c7c455a7b
mahendra-k1 <mahendra4int@gmail.com>
2025-08-08 21:08:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,23 +13,23 @@ variable "location" {
 variable "subscription_id" {
   description = "Azure Subscription ID"
   type        = string
-  
+  default     = "7f028467-e553-46c3-8194-0329abb44de1"
 }
 
 variable "client_id" {
   description = "Azure Service Principal Client ID"
   type        = string
-
+  default     = "ebf54274-562d-449e-9119-d5a1e10d2a32"
 }
 
 variable "client_secret" {
   description = "Azure Service Principal Client Secret"
   type        = string
-
+  default     = "gsg8Q~mx4KQwxID__cJQ0nu8AwNf-p7KaE3xibWx"
 }
 
 variable "tenant_id" {
   description = "Azure Tenant ID"
   type        = string
-  
+  default     = "709a0f80-2390-4f0a-abff-26414bd9bb33"
 }
